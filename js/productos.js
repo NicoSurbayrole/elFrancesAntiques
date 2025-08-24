80980d17fce6faddafa480dff6358c829b3ad7d4
nicolas <nico.surba@hotmail.com>
2025-08-24 18:32:31 -0300

    logica para traer los productos + sacar id especifico

--- js/productos.js
+++ js/productos.js
@@ -1,18 +1,54 @@
-let productos = fetch("../data/Productos.JSON").then((res) => res.json());
+const productos = fetch("../data/Productos.JSON").then((res) => res.json());
 
 const main = document.getElementById("main");
-const section = document.createElement("section");
 
-productos.then(({ productos }) => {
-  console.log(productos);
+const divMain = document.createElement("div");
+divMain.className = "mainContent";
+main.appendChild(divMain);
+
+const aside = document.createElement("aside");
+aside.className = "sidebar";
+divMain.appendChild(aside);
+
+const titelProductos = document.createElement("h2");
+titelProductos.className = "tituloProductos";
+titelProductos.innerHTML = "todos los articulos";
+divMain.appendChild(titelProductos);
+
+mostrarProductos()
+
+function mostrarProductos() {
+  productos.then(({ productos }) => {
+  const section = document.createElement("section");
+   section.className = "cardsContain"
   productos.slice(0, 12).forEach((producto) => {
     section.innerHTML += `
-    <div>
-        <img width="100px" heigth="100px" src=${producto.image}>
-        <h3>${producto.name}</h3>
+    <div class="cardInfo">
+      <img src=${producto.image}>
+      <header class="cardHeader"> 
+        <h3 class="productName">${producto.name}</h3>
         <small>${producto.price}usd</small>
-        <button id="${producto.product_id}">ver producto</button>
+        </header>
+      <footer class="cardFooter">
+        <button class="cardButton" id="${producto.product_id}">ver producto</button>
+      </footer> 
     </div>`;
-    main.appendChild(section);
+    divMain.appendChild(section);
   });
+  singleProduct()
 });
+}
+
+function singleProduct(){
+    let button = document.getElementsByClassName("cardButton")
+  
+  //la idea despues es que el id renderice ese elemento unico y hacerle una vista detallada
+  for(let i = 0; i < button.length; i++){
+    button[i].addEventListener("click", () =>{
+    localStorage.setItem("productId", button[i].id)
+    window.location.assign(`../pages/producto.html`)
+    console.log(`seleccionaste el elemento con el id: ${button[i].id}`)
+    })
+  }
+}
+
